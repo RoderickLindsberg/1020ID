5ddc721ec700bcfd77f992b0a0745f1fde2c7505
rlindsberg <ruliu@kth.se>
2017-09-05 18:22:22 +0200

    An array-cache has been implemented and some of the duplicate operations are skipped. Time for printing out a n = 30 Pascal triangle has been improved from 1.9 s to 20 ms.

--- Main.java
+++ Main.java
@@ -1,4 +1,29 @@
 public class Main {
     public static void main(String[] args) {
+
+        //this is the first test
+        long startTime = System.currentTimeMillis();
+
+        RecursivePascal testPascal = new RecursivePascal();
+        testPascal.printPascal(39);
+        System.out.println();
+
+
+        long stopTime = System.currentTimeMillis();
+        System.out.println("The cache-enabled program takes " + (stopTime - startTime) + " ms.\n");
+
+
+
+        //this is another test
+        startTime = System.currentTimeMillis();
+
+
+        testPascal.printPascalSlowly(29);
+        System.out.println();
+
+
+        stopTime = System.currentTimeMillis();
+        System.out.println("The no-cache program takes " + (stopTime - startTime) + " ms.");
+
     }
 }
--- RecursivePascal.java
+++ RecursivePascal.java
@@ -1,3 +1,84 @@
 public class RecursivePascal {
+    //this variable indicates the line number. It starts at line 0 and prints 1, ... line 3 and prints 1 2 1 etc.
+    public int whichLine = 0;
+    //when false, prints out the triangle upside down.
+    boolean isIncrementing = true;
 
+
+    int[][] myArrayCache = new int[40][40];
+//    RecursivePascal(int n) {
+//        int[][] myArrayCache = new int[n][n];
+//    }
+
+    //this method calculate which number to be print out, when provided "coordinates" n and k
+    public int binom(int n, int k) {
+        if (k == 0 | k == n) {
+            return 1;
+        } else if (k == 1 | n - k == 1) {
+            return n;
+        }
+
+        if (myArrayCache[n][k] != 0){
+            return myArrayCache[n][k];
+        } else {
+            myArrayCache[n][k] = binom(n - 1, k - 1) + binom(n - 1, k);
+            return myArrayCache[n][k];
+        }
+    }
+
+
+
+    //this method prints out a Pascal triangle in a slower way
+    public void printPascal(int n) {
+
+        if (isIncrementing) {
+            if (n == whichLine) {
+                //Prints out all numbers in a line (example 1 2 1 when whichLine == 2)
+                for (int k = 0; k < n + 1; k++) {
+                    System.out.print(binom(n, k) + "\t");
+                }
+                System.out.println();
+                whichLine++;
+
+                //if not, call binom and store the return value in the array
+
+            } else{
+                printPascal(n - 1);
+                printPascal(n);
+            }
+
+        } else
+            //(else if) runs when isIncrementing is false
+            if (n >= 0) {
+                for (int k = 0; k < n + 1; k++) {
+                    System.out.print(binom(n, k) + "\t");
+                }
+                System.out.println();
+                printPascal(n - 1);
+            }
+    }
+
+
+
+
+
+    public static int binomSlowly(int n, int k) {
+        if (k == 0 | k == n) {
+            return 1;
+        } else if (k == 1 | n - k == 1) {
+            return n;
+        } else {
+            return binomSlowly(n - 1, k - 1) + binomSlowly(n - 1, k);
+        }
+    }
+
+    public void printPascalSlowly(int n) {
+        if (n >= 0) {
+            for (int k = 0; k < n + 1; k++) {
+                System.out.print(binomSlowly(n, k) + "\t");
+            }
+            System.out.println();
+            printPascalSlowly(n - 1);
+        }
+    }
 }
