d440313cdc287c0817ccb87509936e7c40892e75
rlindsberg <roderick@karlemstrand.com>
2018-08-04 15:55:58 +0200

    Data types in Java

--- Java Basics/Tutorial1/src/Application.java
+++ Java Basics/Tutorial1/src/Application.java
@@ -1,5 +1,31 @@
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //primitive types in Java
+        //int   32-bit-number
+        int number = 10;
+        //short 16
+        short myShort = 847;
+        //long  64
+        long myLong = 9797;
+        //double
+        double myDouble = 7.4232;
+        //float
+        float myFloat = 3.233f;
+        //char
+        char myChar = 'y';
+        //boolean
+        boolean myBoolean = false;
+        //byte
+        byte myByte = 127;
+
+
+        System.out.println(number);
+        System.out.println(myShort);
+        System.out.println(myLong);
+        System.out.println(myDouble);
+        System.out.println(myFloat);
+        System.out.println(myChar);
+        System.out.println(myBoolean);
+        System.out.println(myByte);
     }
 }
