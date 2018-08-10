45d7230f51feedda648d8569dc7aa3a42e26abb4
rlindsberg <roderick@karlemstrand.com>
2018-08-10 09:51:12 +0200

    Create new input scanner

--- Java Basics/Tutorial1/src/GetUserInput.java
+++ Java Basics/Tutorial1/src/GetUserInput.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
 public class GetUserInput {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        
+        System.out.println("userinput");
+
+    }
 }
