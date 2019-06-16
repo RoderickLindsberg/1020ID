21b39f6d753c9330008114d2abaf5fb29f62e8b3
rlindsberg <roderick@karlemstrand.com>
2019-06-16 11:01:52 +0200

    Done 3. write the method findSimpleGene

--- OOPSpecialization/04_Finding_a_Gene_and_Web_Links/StringFirstAssignment/Part1.java
+++ OOPSpecialization/04_Finding_a_Gene_and_Web_Links/StringFirstAssignment/Part1.java
@@ -1,10 +1,27 @@
-
 /**
  * Write a description of Part1 here.
  *
  * @author RK
  * @version 1.0
  */
-public class Part1 {
 
+import edu.duke.*;
+public class Part1 {
+    public String findSimpleGene (String dna) {
+      int startCodon = dna.indexOf("ATG");
+      int stopCodon = dna.indexOf("TAA",startCodon+3);
+      if ( startCodon ==-1){
+          return "";
+      }
+      if (stopCodon == -1){
+          return "";
+      }
+      return dna.substring(startCodon,stopCodon+3);
+    }
+
+    public static void main (String[] args){
+        Part1 geneFinder = new Part1();
+        String dna = "TTTATGZZZZZZTAA";
+        System.out.println("Gene found: " + geneFinder.findSimpleGene(dna));
+    }
 }
