170cb4551ca3083ef896716877b6b2b13b4410a1
rlindsberg <roderick@karlemstrand.com>
2019-06-17 23:12:36 +0200

    Finish test cases

--- OOPSpecialization/04_Finding_a_Gene_and_Web_Links/StringFirstAssignment/Part1.java
+++ OOPSpecialization/04_Finding_a_Gene_and_Web_Links/StringFirstAssignment/Part1.java
@@ -19,9 +19,28 @@ public class Part1 {
       return dna.substring(startCodon,stopCodon+3);
     }
 
+    // Five DNA strings:
+    //  DNA with no “ATG”
+    //  DNA with no “TAA”
+    //  DNA with no “ATG” or “TAA”
+    //  DNA with ATG, TAA and the substring between them is a multiple of 3 (a gene)
+    //  DNA with ATG, TAA and the substring between them is not a multiple of 3.
+    public void testSimpleGene() {
+        String[] dnaArray = {"ZZZTAAZZZ", "ZZZATGZZZ", "ZZZ", "ZZZATGZTAAZZZ", "ZZZATGZZZTAA"};
+        for (String dna : dnaArray) {
+            System.out.println("Testing DNA string: " + dna);
+
+            String res = findSimpleGene(dna);
+            if (res == "") {
+                System.out.println("Bummer! No gene is found..");
+            } else System.out.println("DNA found: " + res);
+        }
+    }
+
     public static void main (String[] args){
         Part1 geneFinder = new Part1();
         String dna = "TTTATGZZZZZZTAA";
         System.out.println("Gene found: " + geneFinder.findSimpleGene(dna));
+        geneFinder.testSimpleGene();
     }
 }
