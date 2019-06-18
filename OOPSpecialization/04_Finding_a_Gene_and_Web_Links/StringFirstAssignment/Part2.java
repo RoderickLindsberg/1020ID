2dfcdae0d63029b02e0898b1653dadfbd873f9bb
rlindsberg <roderick@karlemstrand.com>
2019-06-18 19:56:40 +0200

    1. New Java Class and 2. Copy and paste the two methods from the Part1

---
+++ OOPSpecialization/04_Finding_a_Gene_and_Web_Links/StringFirstAssignment/Part2.java
@@ -0,0 +1,58 @@
+
+/**
+ * Write a description of Part2 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Part2 {
+    
+    private boolean validateDNA (String dna) {
+        if (dna.length() % 3 == 0) {
+            return true;
+        } else return false;
+    }
+
+    private String findSimpleGene(String dna) {
+        // start codon is ATG
+        // stop codon is TAA
+        int pos;
+        int indexStartCodon = dna.indexOf("ATG");
+        if (indexStartCodon == -1) {
+            return "";
+        }
+
+        int indexStopCodon = dna.indexOf("TAA", indexStartCodon + 3);
+        if (indexStopCodon == -1) {
+            return "";
+        }
+
+        String gene = dna.substring(indexStartCodon, indexStopCodon + 3);
+        if (!validateDNA(gene)) {
+            return "";
+        }
+
+        return gene;
+    }
+    
+    // Five DNA strings:
+    //  DNA with no “ATG”
+    //  DNA with no “TAA”
+    //  DNA with no “ATG” or “TAA”
+    //  DNA with ATG, TAA and the substring between them is a multiple of 3 (a gene)
+    //  DNA with ATG, TAA and the substring between them is not a multiple of 3.
+    public void testSimpleGene() {
+        String[] dnaArray = {"ZZZTAAZZZ", "ZZZATGZZZ", "ZZZ", "ZZZATGZTAAZZZ", "ZZZATGZZZTAA"};
+
+        for (String dna : dnaArray) {
+            System.out.println("Testing DNA string: " + dna);
+
+            // calling findSimpleGene() to get gene
+            String res = findSimpleGene(dna);
+            
+            if (res == "") {
+                System.out.println("Bummer! No gene is found..");
+            } else System.out.println("DNA found: " + res);
+        }
+    }
+}
