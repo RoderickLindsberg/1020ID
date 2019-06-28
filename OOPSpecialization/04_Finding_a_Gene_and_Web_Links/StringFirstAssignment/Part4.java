1a76a1f4ca04330d611781eb15fc561a4bf9d098
rlindsberg <roderick@karlemstrand.com>
2019-06-28 09:25:24 +0200

    Find youtube links and print them out

--- OOPSpecialization/04_Finding_a_Gene_and_Web_Links/StringFirstAssignment/Part4.java
+++ OOPSpecialization/04_Finding_a_Gene_and_Web_Links/StringFirstAssignment/Part4.java
@@ -7,13 +7,15 @@
 import edu.duke.*;
 
 public class Part4 {
-    
+
     public static void main(String[] args) {
         String url = "http://www.dukelearntoprogram.com/course2/data/manylinks.html";
         URLResource ur = new URLResource(url);
 
         for (String word : ur.words()) {
-            System.out.println(word);
+            if (word.indexOf("youtube.com") != -1) {
+                System.out.println(word);
+            }
         }
     }
 }
